ed217f5969fc0f9b9a35614aa5b71f2da270d2cc
EvieCoding <112533060+EvieCoding@users.noreply.github.com>
2023-10-03 14:54:40 -0500

    Add files via upload

---
+++ Dog.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoggyDayCare2
+{
+    public class Dog
+    {
+        private string _dogName,
+            _ownerName,
+            _dogBreed,
+            _dogColor,
+            _size,
+            _lifeStage;
+        private static float _age,
+            _weight;
+        private static double _total;
+        private bool _food;
+        private static int _instanceCount;
+
+        public Dog()
+        {
+            _instanceCount++;
+        }
+
+        public Dog(string pupName, string clientName, float pupAge, string pupBreed,
+            float pupWeight, bool pupFood, string pupColor)
+        {
+            _dogName = pupName;
+            _ownerName = clientName;
+            _age = pupAge;
+            _dogBreed = pupBreed;
+            _weight = pupWeight;
+            _food = pupFood;
+            _dogColor = pupColor;
+            _total = 0.0;
+            _age = 0;
+            _lifeStage = "";
+            _size = "";
+        }
+
+        public int getInstanceCount()
+        {
+            return _instanceCount;
+        }
+
+        public string getDogName()
+        {
+            return _dogName;
+        }
+
+        public string getOwnerName()
+        {
+            return _ownerName;
+        }
+
+        public string getBreed()
+        {
+            return _dogBreed;
+        }
+
+        public string getColor()
+        {
+            return _dogColor;
+        }
+
+        public string getSize()
+        {
+            return _size;
+        }
+
+        public string getLifeStage()
+        {
+            return _lifeStage;
+        }
+
+        public float getAge()
+        {
+            return _age;
+        }
+
+        public float getWeight()
+        {
+            return _weight;
+        }
+
+        public double getTotal()
+        {
+            return _total;
+        }
+
+        public bool getFood()
+        {
+            return _food;
+        }
+
+        public void setDogName(string pupName)
+        {
+            _dogName = pupName;
+        }
+
+        public void setOwnerName(string clientName)
+        {
+            _ownerName = clientName;
+        }
+
+        public void setBreed(string pupBreed)
+        {
+            _dogBreed = pupBreed;
+        }
+
+        public void setColor(string pupColor)
+        {
+            _dogColor = pupColor;
+        }
+
+        public void setAge(float pupAge)
+        {
+            _age = pupAge;
+        }
+
+        public void setWeight(float pupWeight)
+        {
+            _weight = pupWeight;
+        }
+
+        public void setFood(bool pupFood)
+        {
+            _food = pupFood;
+        }
+
+        public string determineLifeStage()
+        {
+            if (_age < 1)
+            {
+                _lifeStage = "puppy";
+            }
+            else if (_age >= 1 && _age <= 3)
+            {
+                _lifeStage = "adolescent";
+            }
+            else if (_age >= 4 && _age <= 8)
+            {
+                _lifeStage = "adult";
+            }
+            else if (_age > 8)
+            {
+                _lifeStage = "senior";
+            }
+            return _lifeStage;
+        }
+
+        public string determineSize()
+        {
+            if (_weight > 0 && _weight <= 10)
+            {
+                _size = "small";
+            }
+            else if (_weight >= 11 && _weight <= 30)
+            {
+                _size = "medium";
+            }
+            else if (_weight >= 31 && _weight <= 100)
+            {
+                _size = "large";
+            }
+            else if (_weight > 100)
+            {
+                _size = "xLarge";
+            }
+
+            return _size;
+        }
+
+        public static void checkOutDog()
+        {
+            _instanceCount--;
+        }
+
+        public static double priceForDay(string size, float pupWeight)
+        {
+            if (size.Equals("small") || size.Equals("medium"))
+            {
+                _total = 45;
+                return _total;
+            }
+            else
+            {
+                _total = 45 + (0.1 * pupWeight);
+                return _total;
+            }
+        }
+
+        public static double priceForDay(bool food)
+        {
+            if (food)
+            {
+                _total += 5;
+            }
+
+            return _total;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoggyDayCare2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int index;
+            float pupAge,
+                pupWeight;
+            string pupName,
+                clientName,
+                pupBreed,
+                pupColor,
+                foodAns;
+            bool pupFood = false;
+            const int NUM_DOGS = 30;
+
+            Console.WriteLine("Select one of the following:\n" +
+                "1 to Check in Dog\n" +
+                "2 to Check out Dog\n" +
+                "3 to Exit\n");
+
+            Dog[] dogs = new Dog[NUM_DOGS];
+
+            Dog dog;
+
+            Console.WriteLine();
+
+            Console.Write("Enter an option: ");
+            var input = Console.ReadLine();
+
+            while (input != "3")
+            {
+                if (input == "1")
+                {
+                    Dog dogInstance = new Dog();
+                    if(dogInstance.getInstanceCount() <= NUM_DOGS)
+                    {
+                        Console.Write("Client's name: ");
+                        clientName = Console.ReadLine();
+
+                        Console.Write("Dog's name: ");
+                        pupName = Console.ReadLine();
+
+                        Console.Write("Dog's breed: ");
+                        pupBreed = Console.ReadLine();
+
+                        Console.Write("Dog's color: ");
+                        pupColor = Console.ReadLine();
+
+                        Console.Write("Dog's age(in years): ");
+                        pupAge = float.Parse(Console.ReadLine());
+
+                        while (pupAge <= 0.0)
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine("Age must be greater than 0.");
+                            Console.Write("Enter another age: ");
+                            pupAge = float.Parse(Console.ReadLine());
+                        }
+
+                        Console.Write("Dog's weight(in pounds): ");
+                        pupWeight = float.Parse(Console.ReadLine());
+
+                        while (pupWeight <= 0.0)
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine("Weight must be greater than 0.");
+                            Console.Write("Enter another weight: ");
+                            pupWeight= float.Parse(Console.ReadLine());
+                        }
+
+                        Console.Write("Did client bring out dog food?(y/n): ");
+                        foodAns = Console.ReadLine();
+
+                        if (foodAns.Equals("y") || foodAns.Equals("Y"))
+                        {
+                            pupFood = true;
+                        }
+                        else
+                        {
+                            pupFood = false;
+                        }
+
+                        Console.WriteLine();
+
+                        dog = new Dog(pupName, clientName, pupAge, pupBreed,
+                            pupWeight, pupFood, pupColor);
+
+                        Dog.priceForDay(dog.determineSize(), pupWeight);
+
+                        Console.WriteLine(String.Format("Total: {0:C}", Dog.priceForDay(pupFood)));
+
+                        index = dogInstance.getInstanceCount() - 1;
+
+                        dogs[index] = dog;
+                        dogs[index].setDogName(pupName);
+                        dogs[index].setOwnerName(clientName);
+                        dogs[index].setBreed(pupBreed);
+                        dogs[index].setColor(pupColor);
+                        dogs[index].setAge(pupAge);
+                        dogs[index].setWeight(pupWeight);
+                        dogs[index].setFood(pupFood);
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Sorry, the Doggy Day Care is at capacity.");
+                    }
+                }
+
+                if (input == "2")
+                {
+                    int count = 0;
+                    Console.Write("Enter the client's name: ");
+                    clientName = Console.ReadLine();
+
+                    Console.WriteLine();
+
+                    for (int Index = 0; Index <= dogs.Length - 1; Index++)
+                    {
+                        if (dogs[Index] != null && dogs[Index].getOwnerName() == clientName)
+                        {
+                            Console.WriteLine(dogs[Index].getDogName() + " is checked out.");
+                            dogs[Index] = null;
+                            Dog.checkOutDog();
+                            count++;
+                        }
+                        else
+                        {
+                            dogs[Index - count] = dogs[Index];
+                        }
+                    }
+
+                    for (int i = 1; i <= count; i++)
+                    {
+                        dogs[dogs.Length - i] = null;
+                    }
+                }
+
+                Console.WriteLine();
+                Console.Write("Enter another selection: ");
+                input = Console.ReadLine();
+            }
+
+            if (input == "3")
+            {
+                DateTime date = DateTime.Now;
+                FileStream file = new FileStream(date.ToString("yyyy-MM-dd") + "LatePickups" + ".txt", FileMode.Create);
+                StreamWriter writer = new StreamWriter(file);
+                
+                for (int INDEX = 0; INDEX < dogs.Length - 1; INDEX++)
+                {
+                    if (dogs[INDEX] != null)
+                    {
+                        writer.WriteLine("Client name: " + dogs[INDEX].getOwnerName());
+                        writer.WriteLine("Dog name: " + dogs[INDEX].getDogName());
+                        writer.WriteLine("Dog Breed: " + dogs[INDEX].getBreed());
+                        writer.WriteLine("Dog Color: " + dogs[INDEX].getColor());
+                        writer.WriteLine("Dog Age: " + dogs[INDEX].getAge());
+                        writer.WriteLine("Dog Weight: " + dogs[INDEX].getWeight());
+
+                        if (dogs[INDEX].getFood() == true)
+                        {
+                            writer.WriteLine("Food: Yes");
+                        }
+                        else
+                        {
+                            writer.WriteLine("Food: No");
+                        }
+                        writer.WriteLine("Lifestage: " + dogs[INDEX].determineLifeStage());
+                        writer.WriteLine("Size: " + dogs[INDEX].getSize());
+                        writer.WriteLine(String.Format("Total: {0:C}",  dogs[INDEX].getTotal()));
+                        writer.WriteLine();
+                    }
+                } 
+
+                writer.Close();
+                Console.WriteLine();
+                Console.WriteLine("The doggy daycare is closed.");
+            }
+        }
+    }
+}
