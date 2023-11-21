bb21d8d673f5578d9605fab9a4909fcd234b6eae
EvieCoding <112533060+EvieCoding@users.noreply.github.com>
2023-11-21 16:35:59 -0600

    Update Program.cs
    Changed case 1 to check for negative case first

--- Program.cs
+++ Program.cs
@@ -34,55 +34,54 @@ namespace DoggyDayCare2
                 switch (input)
                 {
                     case "1":
-                        if (dogs.Count < MAX_DOGS)
-                        {
-                            Console.Write("Client's name: ");
-                            clientName = Console.ReadLine().Trim();
-
-                            Console.Write("Dog's name: ");
-                            pupName = Console.ReadLine().Trim();
-
-                            Console.Write("Dog's breed: ");
-                            pupBreed = Console.ReadLine().Trim();
-
-                            Console.Write("Dog's color: ");
-                            pupColor = Console.ReadLine().Trim();
-
-                            Console.Write("Dog's age(in years): ");
-                            input = Console.ReadLine().Trim();
-
-                            var inputDog = new Dog();
-                            pupAge = inputDog.InvalidInput(input);
-
-                            Console.Write("Dog's weight(in pounds): ");
-                            input = Console.ReadLine().Trim();
-
-                            pupWeight = inputDog.InvalidInput(input);
-
-                            Console.Write("Did client bring dog food?(y/n): ");
-                            input = Console.ReadLine().ToUpper().Trim();
-
-                            input = inputDog.YesNoInputValidation(input);
-
-                            if (input.Equals("Y"))
-                            {
-                                pupFood = true;
-                            }
-
-                            Console.WriteLine();
-
-                            var dog = new Dog(pupName, clientName, pupAge, pupBreed,
-                                pupWeight, pupFood, pupColor);
-                            dogs.Add(dog);
-                            dog.DetermineSize();
-
-                            Console.WriteLine(String.Format("Total: {0:C}", dog.Priceforday()));
-                        }
-                        else
+                        if (dogs.Count == MAX_DOGS)
                         {
                             Console.WriteLine();
                             Console.WriteLine("Sorry, the Doggy Day Care is at capacity.");
+                            break;
                         }
+
+                        Console.Write("Client's name: ");
+                        clientName = Console.ReadLine().Trim();
+
+                        Console.Write("Dog's name: ");
+                        pupName = Console.ReadLine().Trim();
+
+                        Console.Write("Dog's breed: ");
+                        pupBreed = Console.ReadLine().Trim();
+
+                        Console.Write("Dog's color: ");
+                        pupColor = Console.ReadLine().Trim();
+
+                        Console.Write("Dog's age(in years): ");
+                        input = Console.ReadLine().Trim();
+
+                        var inputDog = new Dog();
+                        pupAge = inputDog.InvalidInput(input);
+
+                        Console.Write("Dog's weight(in pounds): ");
+                        input = Console.ReadLine().Trim();
+
+                        pupWeight = inputDog.InvalidInput(input);
+
+                        Console.Write("Did client bring dog food?(y/n): ");
+                        input = Console.ReadLine().ToUpper().Trim();
+
+                        input = inputDog.YesNoInputValidation(input);
+
+                        if (input.Equals("Y"))
+                        {
+                            pupFood = true;
+                        }
+
+                        Console.WriteLine();
+
+                        var dog = new Dog(pupName, clientName, pupAge, pupBreed,
+                            pupWeight, pupFood, pupColor);
+                        dogs.Add(dog);
+                        dog.DetermineSize();
+
+                        Console.WriteLine(String.Format("Total: {0:C}", dog.Priceforday()));
 
                         break;
 
