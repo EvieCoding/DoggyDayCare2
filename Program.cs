7998a4bed10b358fd123109df0b00415de05b095
EvieCoding <112533060+EvieCoding@users.noreply.github.com>
2023-11-21 14:02:22 -0600

    Update Program.cs
    Changed the if/else structure to a switch case. Moved the file writing to a method in the class.

--- Program.cs
+++ Program.cs
@@ -36,93 +36,77 @@ namespace DoggyDayCare2
 
             while (input != "3")
             {
-                if (input == "1")
+                switch (input)
                 {
-                    if (dogs.Count < MAX_DOGS)
-                    {
-                        Console.Write("Client's name: ");
-                        clientName = Console.ReadLine().Trim();
-
-                        Console.Write("Dog's name: ");
-                        pupName = Console.ReadLine().Trim();
-
-                        Console.Write("Dog's breed: ");
-                        pupBreed = Console.ReadLine().Trim();
-
-                        Console.Write("Dog's color: ");
-                        pupColor = Console.ReadLine().Trim();
-
-                        Console.Write("Dog's age(in years): ");
-                        input = Console.ReadLine().Trim();
-
-                        while(!float.TryParse(input, out pupAge) || pupAge <= 0)
+                    case "1":
+                        if (dogs.Count < MAX_DOGS)
                         {
-                            Console.WriteLine("Age must be a number and greater than 0.");
-                            Console.Write("Enter the age again: ");
+                            Console.Write("Client's name: ");
+                            clientName = Console.ReadLine().Trim();
+
+                            Console.Write("Dog's name: ");
+                            pupName = Console.ReadLine().Trim();
+
+                            Console.Write("Dog's breed: ");
+                            pupBreed = Console.ReadLine().Trim();
+
+                            Console.Write("Dog's color: ");
+                            pupColor = Console.ReadLine().Trim();
+
+                            Console.Write("Dog's age(in years): ");
                             input = Console.ReadLine().Trim();
-                        }
 
-                        Console.Write("Dog's weight(in pounds): ");
-                        input = Console.ReadLine().Trim();
+                            var inputDog = new Dog();
+                            pupAge = inputDog.invalidInput(input);
 
-                        while(!float.TryParse(input,out pupWeight) || pupWeight <= 0)
-                        {
-                            Console.WriteLine("Weight must be a number and greater than 0.");
-                            Console.Write("Enter the weight again: ");
+                            Console.Write("Dog's weight(in pounds): ");
                             input = Console.ReadLine().Trim();
-                        }
 
-                        Console.Write("Did client bring dog food?(y/n): ");
-                        input = Console.ReadLine().ToUpper().Trim();
+                            pupWeight = inputDog.invalidInput(input);
 
-                        while (!input.Equals("Y") && !input.Equals("N") || input.Equals(""))
-                        {
-                            Console.WriteLine("Enter y for yes or n for no.");
                             Console.Write("Did client bring dog food?(y/n): ");
-
                             input = Console.ReadLine().ToUpper().Trim();
-                        }
 
-                        if (input.Equals("Y"))
-                        {
-                            pupFood = true;
+                            input = inputDog.yesNoInputValidation(input);
+
+                            if (input.Equals("Y"))
+                            {
+                                pupFood = true;
+                            }
+
+                            Console.WriteLine();
+
+                            var dog = new Dog(pupName, clientName, pupAge, pupBreed,
+                                pupWeight, pupFood, pupColor);
+                            dogs.Add(dog);
+                            dog.DetermineSize();
+
+                            Console.WriteLine(String.Format("Total: {0:C}", dog.Priceforday()));
                         }
                         else
                         {
-                            pupFood = false;
+                            Console.WriteLine();
+                            Console.WriteLine("Sorry, the Doggy Day Care is at capacity.");
                         }
 
+                        break;
+
+                    case "2":
+                        Console.Write("Enter the client's name: ");
+                        clientName = Console.ReadLine().Trim();
+
                         Console.WriteLine();
 
-                        var dog = new Dog(pupName, clientName, pupAge, pupBreed,
-                            pupWeight, pupFood, pupColor);
-                        dogs.Add(dog);
-                        dog.determineSize();
-
-                        Console.WriteLine(String.Format("Total: {0:C}", dog.priceForDay()));
-                    }
-                    else
-                    {
-                        Console.WriteLine();
-                        Console.WriteLine("Sorry, the Doggy Day Care is at capacity.");
-                    }
-                }
-
-                if (input == "2")
-                {
-                    Console.Write("Enter the client's name: ");
-                    clientName = Console.ReadLine().Trim();
-
-                    Console.WriteLine();
-
-                    for (int i = 0; i < dogs.Count; i++)
-                    {
-                        if (dogs[i].ownerName == clientName)
+                        for (int i = 0; i < dogs.Count; i++)
                         {
-                            Console.WriteLine("{0} was checked out.", dogs[i].dogName);
-                            dogs.RemoveAt(i);
+                            if (dogs[i].OwnerName == clientName)
+                            {
+                                Console.WriteLine("{0} was checked out.", dogs[i].DogName);
+                                dogs.RemoveAt(i);
+                            }
                         }
-                    }
+
+                        break;
                 }
 
                 Console.WriteLine();
@@ -130,42 +114,8 @@ namespace DoggyDayCare2
                 input = Console.ReadLine().Trim();
             }
 
-            DateTime date = DateTime.Now;
-            FileStream file = new FileStream(date.ToString("yyyy-MM-dd") + "LatePickups" + ".txt", FileMode.Create);
-            StreamWriter writer = new StreamWriter(file);
-
-            foreach (Dog left in dogs)
-            {
-                if (left != null)
-                {
-                    writer.WriteLine(
-                        "Client name: {0}\n" +
-                        "Dog name: {1}\n" +
-                        "Dog Breed: {2}\n" +
-                        "Dog Color: {3}\n" +
-                        "Dog age: {4}\n" +
-                        "Dog Weight: {5}", left.ownerName, left.dogName, left.dogBreed, left.dogColor, left.age, left.weight);
-
-                    if (left.food == true)
-                    {
-                        writer.WriteLine("Food: Yes");
-                    }
-                    else
-                    {
-                        writer.WriteLine("Food: No");
-                    }
-
-                    writer.WriteLine(
-                        "Lifestage: {0}\n" +
-                        "Size: {1}\n" +
-                        "Total: {2:C}\n", left.determineLifeStage(), left.size, left.total);
-                }
+            Dog.writeToAFile(dogs);
             }
-
-            writer.Close();
-            Console.WriteLine();
-            Console.WriteLine("The doggy daycare is closed.");
-            
         }
     }
 }
